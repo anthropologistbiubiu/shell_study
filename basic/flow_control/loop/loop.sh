0ffe239174ea28525106cfefae0134632524d2e9
anthropologistbiubiu <1319847957@qq.com>
2024-01-14 15:27:44 +0800

    flow control

--- basic/flow_control/loop/loop.sh
+++ basic/flow_control/loop/loop.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 
 # for 循环
+
 for item in {1..5}
 do
   echo $item
@@ -7,13 +10,46 @@ done
 
 # while 循环
 # 打印数字1～5
-
+echo 'while 循环'
 count=1
-while [[count -le 5]]
+while [[ $count -le 5 ]]
 do
   echo $count 
   ((count++))
 done
+
+
+# 循环读取文件内容
+echo '循环读取文件'
+while read line  
+do
+  echo $line
+done < test.txt
+
+# c风格for 循环
+for ((i=0;i<10;i++))
+do
+  echo $i
+done
+
+echo $count
+# 条件语句
+
+if [ $count -le 10 ]
+then 
+  echo 'count < 10' 
+fi
+
+
+
+
+
+
+
+
+
+
+
 
 
 
