3c631b11205334d8705acd2e630d96f73e3db395
anthropologistbiubiu <1319847957@qq.com>
2024-01-15 22:03:59 +0800

    operator

--- basic/operator/logic.sh
+++ basic/operator/logic.sh
@@ -3,23 +3,23 @@
 
 
 # && 运算符
-a=3
-b=1
+a=-2
+b=5
 if (( $a > 1 )) && [ $b -lt 2 ]
 then
-    echo "a > 1 && b > 2"
+    echo "a > 1 && b < 2"
 elif [ $a -gt 1 ] || [ $b -le 2 ]
 then
-    echo " a > 1 || b >= 2 "
+    echo " a > 1 || b <= 2 "
 elif (( a == 1)) && (( b== 2))
 then
     echo "a==1 && b==2"
 elif [ $a -gt 1 ] && [ $b -lt 2 ]
 then
-    echo "a > 1 && b >2"
-elif [ $a -ge 1 ] || [ $b -gt 2 ] 
+    echo "a > 1 && b <2 "
+elif ! [ $a -gt 0 ] && [ $b -gt 0 ] 
 then
-    echo "a >=1 && b >2" 
+    echo "a < 0 && b < 2" 
 else 
     echo "else"
 fi
