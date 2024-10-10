278a1d3e0af0c0592030591f5d5c2045ffae8469
anthropologistbiubiu <1319847957@qq.com>
2024-10-10 23:25:03 +0800

    study

--- basic/practices/day02.sh
+++ basic/practices/day02.sh
@@ -4,9 +4,11 @@
 Name=httpd
 
 Num=$(ps -ef | grep $Name | grep -vc grep)
+# grep -v grep 表示从结果当中过滤掉包含 grep 的记录
+# grep -c 表示统计行数
 
-if [ $Num -eq 1 ]; then
-    echo " $Name is running"
-else 
+if [ $Num -eq 1 ];then 
+    echo "$Name is runnning"
+else
     echo "$Name is not running"
 fi
