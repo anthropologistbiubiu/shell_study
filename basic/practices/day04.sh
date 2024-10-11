fe77eed183c79e037655b3f09b73ecac99b02715
anthropologistbiubiu <1319847957@qq.com>
2024-10-11 22:15:34 +0800

    study

---
+++ basic/practices/day04.sh
@@ -0,0 +1,17 @@
+
+
+# 判断用户是否存在
+
+
+
+read -p "请输入用户名" user
+
+for i in  `awk -F : '{print $1}' /etc/passwd`
+
+do
+    if [ i == "$user" ];then
+        echo "$user exists"
+    else
+        echo "$user not exists"
+    fi
+done
