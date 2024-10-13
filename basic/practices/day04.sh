3b7b72f4c97192aeccbdd1144fd2c57d898e577b
anthropologistbiubiu <1319847957@qq.com>
2024-10-13 22:42:27 +0800

    study

--- basic/practices/day04.sh
+++ basic/practices/day04.sh
@@ -6,8 +6,7 @@
 
 read -p "请输入用户名" user
 
-for i in  `awk -F : '{print $1}' /etc/passwd`
-
+for i in  $(awk -F : '{print $1}' /etc/passwd)
 do
     if [ i == "$user" ];then
         echo "$user exists"
---
+++ basic/practices/day06.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "the script name is: $0"
+echo "the first argument  is: $1"
+echo "the first argument  is: $2"
+echo "numbers of arguments is: $#"
