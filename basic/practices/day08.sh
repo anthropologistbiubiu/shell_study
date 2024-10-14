58e7dcc5954b3440512e367a68341eaf301c6a7c
anthropologistbiubiu <1319847957@qq.com>
2024-10-14 23:59:11 +0800

    study

--- basic/practices/day08.sh
+++ basic/practices/day08.sh
@@ -5,13 +5,24 @@
 
 
 
-HOSTS="103.235.47.188 103.235.46.96"
+HOSTS="103.235.47.188 103.235.46.96 127.0.0.1 123.123.123.123"
 
 for HOST in $HOSTS
 do
-    if ping -c 1 $HOST > /dev/null;then
-        echo "$HOST exist"
-    else
-        echo "$HOST not exists"
-    fi
+    NUM=1
+    while [ $NUM -le 3 ]; do
+        if ping -c 1 $HOST > /dev/null;then
+            echo "$HOST exist"
+            break
+        else
+            echo "$HOST not exists"
+            NUM=$((NUM+1))
+        fi
+
+        if [ $NUM -gt 3 ]; then
+            echo "$HOST cant reach"
+        fi
+        
+    done
+    
 done
