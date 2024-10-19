ec8dbf7eb0baf3a1fd7d16a1eb6fb87d20338049
anthropologistbiubiu <1319847957@qq.com>
2024-10-19 22:30:45 +0800

    study

--- basic/practices/day13.sh
+++ basic/practices/day13.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 统计所有进程占用的内存和
-
-
-ps -e -o pid,user,%mem --sort=-%mem
+# 统计所有进程占用的内存百分比的和
+ps -e -o %mem | awk '{sum+=$1} END {print sum}'
+# 统计所有进程占用的内存的和
+ps -e -o rss | awk '{sum+=$1} END {print sum/1024}'
