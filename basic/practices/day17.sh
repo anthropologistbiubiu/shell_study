d3b99770d2547e8552dc8d146af800ae816f91db
anthropologistbiubiu <1319847957@qq.com>
2024-10-20 19:51:19 +0800

    study

--- basic/practices/day17.sh
+++ basic/practices/day17.sh
@@ -2,4 +2,14 @@
 
 
 
-awk '{}'
+awk '{ 
+    for (i=1;i<=NF;i++) {
+        word=tolower($i)
+        count[word]++
+    }
+}
+    END {
+        for ( w in count ) {
+            print w,count[w]
+        }
+}' $1 | sort -k2,2n
