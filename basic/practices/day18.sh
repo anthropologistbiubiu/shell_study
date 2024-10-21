cbb8e5f41519b28061e47ccda89e266f52d5d84c
anthropologistbiubiu <1319847957@qq.com>
2024-10-21 23:19:06 +0800

    study

--- basic/practices/day18.sh
+++ basic/practices/day18.sh
@@ -1,2 +1,18 @@
-#!/bash/bin
+#!/bin/bash
+
+
+
+if [[ ! -f "$1" ]];then
+    echo "文件不存在"
+    exit 1
+fi
+
+awk -F ":" '{
+    line=""
+    for (i=NF;i>1;i--){
+        line = line $i ":"
+    }
+    line=line $1
+    print line
+}' "$1"
 
