2e2a204cb5272efc408a9384d4ac8935c31b3221
anthropologistbiubiu <1319847957@qq.com>
2024-10-22 23:48:58 +0800

    study

--- basic/practices/day19.sh
+++ basic/practices/day19.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 
-awk '$6="ESTABLISHED" && $5 ~ /:3306$/ ' $1 | cut -d ":" -f 1 | uniq -c  | sort -nr
+# awk '$6="ESTABLISHED" && $5 ~ /:3306$/ ' $1 | cut -d ":" -f 1 | sort | uniq -c  | sort -nr | awk '{print $1 " " $5}'
+#awk '$6="ESTABLISHED" && $5 ~ /:3306$/ ' $1 | cut -d ":" -f 1 | sort | uniq -c  | sort -nr 
+
+#awk '$6="ESTABLISHED" && $5 ~ /:3306$/ ' $1 | cut -d ":" -f 1 
+awk '$6="ESTABLISHED" && $5 ~ /:3306$/ ' $1  | cut -d ":" -f 2 | cut -d " " -f 2 | sort | uniq -c | sort -nr  | awk '{print$1 " "$2}'
