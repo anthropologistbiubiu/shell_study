3da6e3cb7eb04f18b81d62873435b4795f3dd70b
anthropologistbiubiu <1319847957@qq.com>
2024-10-23 22:48:00 +0800

    study

---
+++ basic/practices/day20.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+awk '
+BEGIN {
+    VSZ_SUM=0
+    RSS_SUM=0
+}
+NR > 1 {
+    VSZ_SUM+=$5
+    RSS_SUM+=$6
+}
+printf("MEM TOTAL\nVSZ_SUM:%.1fM,RSS_SUM:%.3fM\n", VSZ_SUM/1024, RSS_SUM_MB/1024);
+' $1
