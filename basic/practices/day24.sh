c53869761a3d78e911df697a8c78bf41c8452760
anthropologistbiubiu <1319847957@qq.com>
2024-11-13 22:38:16 +0800

    study

--- basic/practices/day24.sh
+++ basic/practices/day24.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+
+#!/bin/bash
+
+# 使用 awk 提取第二列信息，并用 sort 和 uniq -c 统计重复次数
+# 使用 sort 先按次数降序排列，再按单词字母升序排列
+# 筛选出重复的项，并格式化输出
+
+awk '{print $2}' newcoder.txt | sort | uniq -c | sort -k1,1nr -k2 | awk ' $1 > 1 {print$1, $2}'
