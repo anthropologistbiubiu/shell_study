b1e05852d158be5bccfe0348b8ddf7fb45a6f223
anthropologistbiubiu <1319847957@qq.com>
2024-11-20 00:13:42 +0800

    study

--- basic/practices/day31.sh
+++ basic/practices/day31.sh
@@ -2,5 +2,11 @@
 
 
 
+total_link=$(grep -i "ESTABLISHED" net.txt | grep "3306"| awk  '{ print $5 }' | awk -F ":" '$2=3306 {print $1}' | wc -l | awk '{print $1}')  
 
-grep -nir "ESTABLISHED" net.txt | awk  '{ print $5 }'   
+total_ip=$(grep -i "ESTABLISHED" net.txt | grep "3306" | awk  '{ print $5 }' | awk -F ":" '$2=3306 {print $1}' | sort | uniq -c  | wc -l | awk '{ print $1}')
+
+
+echo "TOTAL_IP $total_ip"
+echo "ESTABLISHED $total_link"
+echo "TOTAL_LINK $total_link"
