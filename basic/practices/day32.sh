e9f9742e0e036215b2d53e7b94452a84674f6298
anthropologistbiubiu <1319847957@qq.com>
2024-11-20 01:28:35 +0800

    study

--- basic/practices/day32.sh
+++ basic/practices/day32.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
-while read -r line;do 
-   echo "$line" 
-done < ip_match.txt
+
+awk '{
+  split($0,parts,".") 
+  if (length(parts) != 4){
+     print "err"
+     next 
+  }
+  for (i in parts) {
+   print i
+   if ( parts[i] !~ /^[0-9]{1,3}$/ || parts[i] < 0 || parts[i] > 255 ){
+      print "no"
+      next 
+   }
+  }
+  print "yes"
+}' ip_match.txt
